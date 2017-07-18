a28a389df8961dde377ec6e56809e761135981c3
Colin Hart <me@colin-hart.com>
2017-07-18 14:21:44 -0400

    added message thread and messages routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :message_thread do
+    resource :messages, only: [':index', ':create']
+  end
+  # resources :messge_threads, only: [':index', ':edit']
 end
