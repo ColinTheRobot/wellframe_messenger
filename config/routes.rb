fcd47fbcbae126138118332fdaf131486a8c81d6
Colin Hart <me@colin-hart.com>
2017-07-18 14:22:38 -0400

    added todo regarding message_thread routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # TODO Further Restrict message_thread block to not create all HTTP routes
   resources :message_thread do
     resource :messages, only: [':index', ':create']
   end
-  # resources :messge_threads, only: [':index', ':edit']
 end
